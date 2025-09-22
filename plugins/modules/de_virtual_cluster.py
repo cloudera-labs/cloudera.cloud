cef0f233b261fba263267e01d0f62093634f2986
Jim Enright <jenright@cloudera.com>
2025-09-22 16:03:11 +0100

    Add vc tier parameter to de_virtual_cluster module (#191)
    Signed-off-by: Jim Enright <jenright@cloudera.com>
    Co-authored-by: Philippe Lanckvrind <planckvrind@cloudera.com>

--- plugins/modules/de_virtual_cluster.py
+++ plugins/modules/de_virtual_cluster.py
@@ -61,6 +61,18 @@ options:
       - Comma-separated Workload usernames of CDP users to be granted access to the Virtual Cluster.
     type: str
     required: False
+  tier:
+    description:
+      - Tier of the virtual cluster, for CDE 1.19.0 and beyond.
+      - C(CORE) enables operational batch jobs.
+      - C(ALLP) enables both operational batch jobs and interactive sessions.
+    type: str
+    required: False
+    choices:
+      - ALLP
+      - CORE
+    aliases:
+      - vc_tier
   chart_value_overrides:
     description:
     - Chart overrides for creating a virtual cluster.
@@ -114,6 +126,7 @@ EXAMPLES = r"""
     name: virtual-cluster-name
     cluster_name: cde-service-name
     env: cdp-environment-name
+    tier: ALLP
     state: present
     wait: true
     delay: 30
@@ -259,6 +272,7 @@ class DEVirtualCluster(CdpModule):
         self.runtime_spot_component = self._get_param("runtime_spot_component")
         self.spark_version = self._get_param("spark_version")
         self.acl_users = self._get_param("acl_users")
+        self.tier = self._get_param("tier")
 
         self.state = self._get_param("state")
         self.force = self._get_param("force")
@@ -385,6 +399,7 @@ class DEVirtualCluster(CdpModule):
             runtime_spot_component=self.runtime_spot_component,
             spark_version=self.spark_version,
             acl_users=self.acl_users,
+            vc_tier=self.tier,
         )
         return_desc = None
         if result and result["vcId"]:
@@ -444,6 +459,13 @@ def main():
             runtime_spot_component=dict(required=False, type="str", default=None),
             spark_version=dict(required=False, type="str", default=None),
             acl_users=dict(required=False, type="str", default=None),
+            tier=dict(
+                required=False,
+                type="str",
+                choices=["ALLP", "CORE"],
+                default=None,
+                aliases=["vc_tier"],
+            ),
             state=dict(
                 required=False,
                 type="str",
