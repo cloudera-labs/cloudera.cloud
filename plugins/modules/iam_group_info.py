059d633d33ccdd6f9d83d88c8007c72872f4c8bf
rsuplina <rsuplina@cloudera.com>
2025-11-19 15:24:11 +0000

    Add note
    Signed-off-by: rsuplina <rsuplina@cloudera.com>

--- plugins/modules/iam_group_info.py
+++ plugins/modules/iam_group_info.py
@@ -158,12 +158,13 @@ class IAMGroupInfo(ServicesModule):
 
     def process(self):
         client = CdpIamClient(api_client=self.api_client)
-
         result = client.list_groups(group_names=self.name)
         self.groups = result.get("groups", [])
-        # self.groups = [
-        #      camel_dict_to_snake_dict(group) for group in result.get("groups", [])
-        # ]
+
+# NOTE: Snake_case conversion deferred until 4.0 to maintain backward compatibility.
+      # self.groups = [
+      #     camel_dict_to_snake_dict(group) for group in result.get("groups", [])
+      # ]
 
 
 def main():
