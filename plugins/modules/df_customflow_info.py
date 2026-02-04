299618ce0a473f65c98c1495fbae97a3868d0cd0
rsuplina <rsuplina@cloudera.com>
2026-02-04 11:40:30 +0000

    Refactor df_customflow_info module
    Signed-off-by: rsuplina <rsuplina@cloudera.com>

--- plugins/modules/df_customflow_info.py
+++ plugins/modules/df_customflow_info.py
@@ -17,178 +17,224 @@
 
 DOCUMENTATION = r"""
 module: df_customflow_info
-short_description: Gather information about CDP DataFlow CustomFlow Definitions
+short_description: Gather information about CDP DataFlow Custom Flow Definitions
 description:
-    - Gather information about CDP DataFlow CustomFlow Definitions
+    - Gather information about CDP DataFlow Custom Flow Definitions
+    - Custom flows are user-created flow definitions uploaded to the DataFlow catalog
 author:
   - "Dan Chaffelson (@chaffelson)"
+  - "Ronald Suplina (@rsuplina)"
 version_added: "1.6.0"
-requirements:
-  - cdpy
 options:
-  name:
+  search_term:
     description:
-      - If a name is provided, that DataFlow Flow Definition will be described
+      - If a search term is provided, flows matching this term will be returned
+      - Searches by flow name
     type: str
     required: False
+    aliases:
+      - name
   include_details:
     description:
       - If set to false, only a summary of each flow is returned
+      - If set to true, detailed information including versions is returned
     type: bool
     required: False
-    default: True
-notes:
-  - The feature this module is for is in Technical Preview
+    default: False
 extends_documentation_fragment:
-  - cloudera.cloud.cdp_sdk_options
-  - cloudera.cloud.cdp_auth_options
+  - cloudera.cloud.cdp_client
 """
 
 EXAMPLES = r"""
 # Note: These examples do not set authentication details.
 
-# List summary information about all Custom DataFlow Flow Definitions
+# List basic information about all DataFlow Custom Flows (summary only)
 - cloudera.cloud.df_customflow_info:
 
-# Gather summary information about a specific DataFlow Flow Definition using a name
+# Search for flows matching a name (summary only)
 - cloudera.cloud.df_customflow_info:
-    name: my-flow-name
-    include_details: false
+    search_term: my-custom-flow
+
+# Search for flows matching a name with detailed information
+- cloudera.cloud.df_customflow_info:
+    search_term: my-custom-flow
+    include_details: true
+
+# Using the 'name' alias (backward compatibility)
+- cloudera.cloud.df_customflow_info:
+    name: my-custom-flow
 """
 
 RETURN = r"""
 flows:
-  description: The listing of CustomFlow Definitions in the DataFlow Catalog in this CDP Tenant
+  description: The information about the named DataFlow Custom Flow or DataFlow Custom Flows
   type: list
   returned: always
   elements: complex
   contains:
     crn:
-      description:  The DataFlow Flow Definition's CRN.
+      description: The artifact CRN
       returned: always
       type: str
     name:
-      description: The DataFlow Flow Definition's name.
-      returned: always
-      type: str
-    modifiedTimestamp:
-      description: The timestamp the entry was last modified.
-      returned: always
-      type: int
-    versionCount:
-      description: The number of versions uploaded to the catalog.
+      description: The artifact name
       returned: always
       type: str
     artifactType:
       description: The type of artifact
+      returned: always
       type: str
-      returned: when include_details is False
+    versionCount:
+      description: The number of versions uploaded to the catalog
+      returned: always
+      type: int
+    modifiedTimestamp:
+      description: The modified timestamp (milliseconds since epoch)
+      returned: when available
+      type: int
+    collectionCrn:
+      description: The collection CRN
+      returned: when available
+      type: str
+    collectionName:
+      description: The collection name
+      returned: when available
+      type: str
     createdTimestamp:
-      description: The created timestamp.
+      description: The created timestamp (milliseconds since epoch)
       returned: when include_details is True
       type: int
     author:
-      description: Author of the most recent version.
+      description: Author of the most recent version
       returned: when include_details is True
       type: str
     description:
-      description: The artifact description.
+      description: The artifact description
       returned: when include_details is True
       type: str
     versions:
-      description: The list of artifactDetail versions.
+      description: The list of flow versions
       returned: when include_details is True
       type: list
+      elements: dict
       contains:
         crn:
-          description: The flow version CRN.
-          returned: when include_details is True
+          description: The flow version CRN
+          returned: always
           type: str
         bucketIdentifier:
-          description: The bucketIdentifier of the flow.
-          returned: when include_details is True
+          description: The bucketIdentifier of the flow
+          returned: when available
           type: str
         author:
-          description: The author of the flow.
-          returned: when include_details is True
+          description: The author of the flow
+          returned: when available
           type: str
         version:
-          description: The version of the flow.
-          returned: when include_details is True
+          description: The version of the flow
+          returned: when available
           type: int
         timestamp:
-          description: The timestamp of the flow.
-          returned: when include_details is True
+          description: The timestamp of the flow (milliseconds since epoch)
+          returned: when available
           type: int
         deploymentCount:
-          description: The number of deployments of the flow.
-          returned: when include_details is True
+          description: The number of deployments of the flow
+          returned: when available
           type: int
+        draftCount:
+          description: The number of draft flows associated with the version
+          returned: when available
+          type: int
+        tags:
+          description: The list of tags associated with the flow version
+          returned: when available
+          type: list
+          elements: dict
+          contains:
+            tagName:
+              description: The name of the version tag
+              returned: always
+              type: str
+            tagColor:
+              description: The color of the version tag
+              returned: when available
+              type: str
         comments:
-          description: Comments about the flow.
-          returned: when include_details is True
+          description: Comments about the flow
+          returned: when available
           type: str
 sdk_out:
-  description: Returns the captured CDP SDK log.
+  description: Returns the captured API HTTP log.
   returned: when supported
   type: str
 sdk_out_lines:
-  description: Returns a list of each line of the captured CDP SDK log.
+  description: Returns a list of each line of the captured API HTTP log.
   returned: when supported
   type: list
   elements: str
 """
 
-from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.cloudera.cloud.plugins.module_utils.cdp_common import CdpModule
+from typing import Any, Dict
+
+from ansible_collections.cloudera.cloud.plugins.module_utils.common import (
+    ServicesModule,
+)
+from ansible_collections.cloudera.cloud.plugins.module_utils.cdp_df import (
+    CdpDfClient,
+)
 
 
-class DFCustomFlowInfo(CdpModule):
-    def __init__(self, module):
-        super(DFCustomFlowInfo, self).__init__(module)
+class DFCustomFlowInfo(ServicesModule):
+    def __init__(self):
+        super().__init__(
+            argument_spec=dict(
+                search_term=dict(required=False, type="str", aliases=["name"]),
+                include_details=dict(required=False, type="bool", default=False),
+            ),
+            supports_check_mode=True,
+        )
 
-        # Set variables
-        self.name = self._get_param("name")
-        self.include_details = self._get_param("include_details")
-
-        # Initialize internal values
-        self.listing = []
+        # Set parameters
+        self.search_term = self.get_param("search_term")
+        self.include_details = self.get_param("include_details")
 
         # Initialize return values
         self.flows = []
 
-        # Execute logic process
-        self.process()
-
-    @CdpModule._Decorators.process_debug
     def process(self):
-        self.listing = self.cdpy.df.list_flow_definitions(name=self.name)
+        client = CdpDfClient(api_client=self.api_client)
+
+        response = client.list_flow_definitions(search_term=self.search_term)
+        flows = response.get("flows", [])
+
         if self.include_details:
-            self.flows = [
-                self.cdpy.df.describe_customflow(x["crn"])
-                for x in self.listing
-                if x["artifactType"] == "flow"  # ReadyFlow have different fields
-            ]
+            for flow_details in flows:
+                if flow_details.get("artifactType") == "flow":
+                    flow_detail = client.describe_flow(flow_details["crn"])
+                    if flow_detail:
+                        self.flows.append(flow_detail.get("flow", flow_details))
+                else:
+                    self.flows.append(flow_details)
         else:
-            self.flows = self.listing
+            self.flows = flows
 
 
 def main():
-    module = AnsibleModule(
-        argument_spec=CdpModule.argument_spec(
-            name=dict(required=False, type="str"),
-            include_details=dict(required=False, type="bool", default=True),
-        ),
-        supports_check_mode=True,
+    result = DFCustomFlowInfo()
+
+    output: Dict[str, Any] = dict(
+        changed=False,
+        flows=result.flows,
     )
 
-    result = DFCustomFlowInfo(module)
-    output = dict(changed=False, flows=result.flows)
+    if result.debug_log:
+        output.update(
+            sdk_out=result.log_out,
+            sdk_out_lines=result.log_lines,
+        )
 
-    if result.debug:
-        output.update(sdk_out=result.log_out, sdk_out_lines=result.log_lines)
-
-    module.exit_json(**output)
+    result.module.exit_json(**output)
 
 
 if __name__ == "__main__":
---
+++ tests/unit/plugins/modules/df_custom_flow_info/test_df_custom_flow_info.py
@@ -0,0 +1,450 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2026 Cloudera, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+import pytest
+
+from ansible_collections.cloudera.cloud.tests.unit import (
+    AnsibleExitJson,
+)
+
+from ansible_collections.cloudera.cloud.plugins.modules import df_customflow_info
+
+
+BASE_URL = "https://cloudera.internal/api"
+ACCESS_KEY = "test-access-key"
+PRIVATE_KEY = "test-private-key"
+FILE_ACCESS_KEY = "file-access-key"
+FILE_PRIVATE_KEY = "file-private-key"
+FILE_REGION = "default"
+
+FLOW_NAME = "my-custom-flow"
+FLOW_CRN = "crn:cdp:df:us-west-1:cloudera:flow:my-custom-flow-id"
+
+
+def test_df_customflow_info_list_all(module_args, mocker):
+    """Test df_customflow_info module with no parameters returns all flows (summary by default)."""
+
+    module_args(
+        {
+            "endpoint": BASE_URL,
+            "access_key": ACCESS_KEY,
+            "private_key": PRIVATE_KEY,
+        },
+    )
+
+    # Patch load_cdp_config to avoid reading real config files
+    config = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.module_utils.common.load_cdp_config",
+    )
+    config.return_value = (FILE_ACCESS_KEY, FILE_PRIVATE_KEY, FILE_REGION)
+
+    # Patch CdpDfClient to avoid real API calls
+    client = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.modules.df_customflow_info.CdpDfClient",
+        autospec=True,
+    ).return_value
+
+    # Mock list_flow_definitions response
+    client.list_flow_definitions.return_value = {
+        "flows": [
+            {
+                "crn": "crn:cdp:df:us-west-1:cloudera:flow:flow1",
+                "name": "Flow One",
+                "artifactType": "flow",
+                "versionCount": 2,
+                "modifiedTimestamp": 1609459200000,
+            },
+            {
+                "crn": "crn:cdp:df:us-west-1:cloudera:flow:flow2",
+                "name": "Flow Two",
+                "artifactType": "flow",
+                "versionCount": 1,
+                "modifiedTimestamp": 1609459300000,
+            },
+        ],
+    }
+
+    # Test module execution
+    with pytest.raises(AnsibleExitJson) as result:
+        df_customflow_info.main()
+
+    assert result.value.changed is False
+    assert len(result.value.flows) == 2
+    assert result.value.flows[0]["name"] == "Flow One"
+    assert result.value.flows[0]["versionCount"] == 2
+    
+    # With include_details=false (default), should not have detailed fields
+    assert "description" not in result.value.flows[0]
+    assert "versions" not in result.value.flows[0]
+
+    # Verify CdpDfClient was called correctly
+    client.list_flow_definitions.assert_called_once()
+    client.describe_flow.assert_not_called()
+
+
+def test_df_customflow_info_list_all_with_details(module_args, mocker):
+    """Test df_customflow_info module with include_details=true returns detailed flow information."""
+
+    module_args(
+        {
+            "endpoint": BASE_URL,
+            "access_key": ACCESS_KEY,
+            "private_key": PRIVATE_KEY,
+            "include_details": True,
+        },
+    )
+
+    # Patch load_cdp_config to avoid reading real config files
+    config = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.module_utils.common.load_cdp_config",
+    )
+    config.return_value = (FILE_ACCESS_KEY, FILE_PRIVATE_KEY, FILE_REGION)
+
+    # Patch CdpDfClient to avoid real API calls
+    client = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.modules.df_customflow_info.CdpDfClient",
+        autospec=True,
+    ).return_value
+
+    # Mock list_flow_definitions response
+    client.list_flow_definitions.return_value = {
+        "flows": [
+            {
+                "crn": "crn:cdp:df:us-west-1:cloudera:flow:flow1",
+                "name": "Flow One",
+                "artifactType": "flow",
+                "versionCount": 2,
+                "modifiedTimestamp": 1609459200000,
+            },
+            {
+                "crn": "crn:cdp:df:us-west-1:cloudera:flow:flow2",
+                "name": "Flow Two",
+                "artifactType": "flow",
+                "versionCount": 1,
+                "modifiedTimestamp": 1609459300000,
+            },
+        ],
+    }
+    
+    # Mock describe_flow responses
+    def describe_flow_side_effect(crn):
+        if "flow1" in crn:
+            return {
+                "flowDetail": {
+                    "crn": crn,
+                    "name": "Flow One",
+                    "artifactType": "flow",
+                    "versionCount": 2,
+                    "modifiedTimestamp": 1609459200000,
+                    "description": "Detailed description of Flow One",
+                    "versions": [{"version": 1}, {"version": 2}],
+                },
+            }
+        elif "flow2" in crn:
+            return {
+                "flowDetail": {
+                    "crn": crn,
+                    "name": "Flow Two",
+                    "artifactType": "flow",
+                    "versionCount": 1,
+                    "modifiedTimestamp": 1609459300000,
+                    "description": "Detailed description of Flow Two",
+                    "versions": [{"version": 1}],
+                },
+            }
+        return None
+    
+    client.describe_flow.side_effect = describe_flow_side_effect
+
+    # Test module execution
+    with pytest.raises(AnsibleExitJson) as result:
+        df_customflow_info.main()
+
+    assert result.value.changed is False
+    assert len(result.value.flows) == 2
+    
+    # Verify detailed fields are present
+    assert result.value.flows[0]["description"] == "Detailed description of Flow One"
+    assert "versions" in result.value.flows[0]
+    assert len(result.value.flows[0]["versions"]) == 2
+    
+    assert result.value.flows[1]["description"] == "Detailed description of Flow Two"
+    assert "versions" in result.value.flows[1]
+    assert len(result.value.flows[1]["versions"]) == 1
+
+    # Verify CdpDfClient was called correctly
+    client.list_flow_definitions.assert_called_once()
+    # Should call describe_flow for each flow
+    assert client.describe_flow.call_count == 2
+
+
+def test_df_customflow_info_by_name(module_args, mocker):
+    """Test df_customflow_info module filtering by name without details."""
+
+    module_args(
+        {
+            "endpoint": BASE_URL,
+            "access_key": ACCESS_KEY,
+            "private_key": PRIVATE_KEY,
+            "name": FLOW_NAME,
+        },
+    )
+
+    # Patch load_cdp_config to avoid reading real config files
+    config = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.module_utils.common.load_cdp_config",
+    )
+    config.return_value = (FILE_ACCESS_KEY, FILE_PRIVATE_KEY, FILE_REGION)
+
+    # Patch CdpDfClient to avoid real API calls
+    client = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.modules.df_customflow_info.CdpDfClient",
+        autospec=True,
+    ).return_value
+
+    # Mock list_flow_definitions response with search term
+    client.list_flow_definitions.return_value = {
+        "flows": [
+            {
+                "crn": FLOW_CRN,
+                "name": FLOW_NAME,
+                "artifactType": "flow",
+                "versionCount": 3,
+                "modifiedTimestamp": 1609459200000,
+            },
+        ],
+    }
+
+    # Test module execution
+    with pytest.raises(AnsibleExitJson) as result:
+        df_customflow_info.main()
+
+    assert result.value.changed is False
+    assert len(result.value.flows) == 1
+    assert result.value.flows[0]["name"] == FLOW_NAME
+    assert result.value.flows[0]["crn"] == FLOW_CRN
+    assert result.value.flows[0]["versionCount"] == 3
+    
+    # Without include_details, should not have detailed fields
+    assert "description" not in result.value.flows[0]
+    assert "versions" not in result.value.flows[0]
+
+    # Verify CdpDfClient was called correctly
+    client.list_flow_definitions.assert_called_once_with(search_term=FLOW_NAME)
+    client.describe_flow.assert_not_called()
+
+
+def test_df_customflow_info_by_name_with_details(module_args, mocker):
+    """Test df_customflow_info module filtering by name with details."""
+
+    module_args(
+        {
+            "endpoint": BASE_URL,
+            "access_key": ACCESS_KEY,
+            "private_key": PRIVATE_KEY,
+            "name": FLOW_NAME,
+            "include_details": True,
+        },
+    )
+
+    # Patch load_cdp_config to avoid reading real config files
+    config = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.module_utils.common.load_cdp_config",
+    )
+    config.return_value = (FILE_ACCESS_KEY, FILE_PRIVATE_KEY, FILE_REGION)
+
+    # Patch CdpDfClient to avoid real API calls
+    client = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.modules.df_customflow_info.CdpDfClient",
+        autospec=True,
+    ).return_value
+
+    # Mock list_flow_definitions response with search term
+    client.list_flow_definitions.return_value = {
+        "flows": [
+            {
+                "crn": FLOW_CRN,
+                "name": FLOW_NAME,
+                "artifactType": "flow",
+                "versionCount": 3,
+                "modifiedTimestamp": 1609459200000,
+            },
+        ],
+    }
+    
+    # Mock describe_flow response
+    client.describe_flow.return_value = {
+        "flowDetail": {
+            "crn": FLOW_CRN,
+            "name": FLOW_NAME,
+            "artifactType": "flow",
+            "versionCount": 3,
+            "modifiedTimestamp": 1609459200000,
+            "description": "A custom flow for data processing",
+            "versions": [{"version": 1}, {"version": 2}, {"version": 3}],
+        },
+    }
+
+    # Test module execution
+    with pytest.raises(AnsibleExitJson) as result:
+        df_customflow_info.main()
+
+    assert result.value.changed is False
+    assert len(result.value.flows) == 1
+    assert result.value.flows[0]["name"] == FLOW_NAME
+    assert result.value.flows[0]["crn"] == FLOW_CRN
+    assert result.value.flows[0]["versionCount"] == 3
+    assert result.value.flows[0]["description"] == "A custom flow for data processing"
+    assert "versions" in result.value.flows[0]
+
+    # Verify CdpDfClient was called correctly
+    client.list_flow_definitions.assert_called_once_with(search_term=FLOW_NAME)
+    client.describe_flow.assert_called_once_with(FLOW_CRN)
+
+
+def test_df_customflow_info_not_found_by_name(module_args, mocker):
+    """Test df_customflow_info module when flow name is not found."""
+
+    module_args(
+        {
+            "endpoint": BASE_URL,
+            "access_key": ACCESS_KEY,
+            "private_key": PRIVATE_KEY,
+            "name": "NonexistentFlow",
+        },
+    )
+
+    # Patch load_cdp_config to avoid reading real config files
+    config = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.module_utils.common.load_cdp_config",
+    )
+    config.return_value = (FILE_ACCESS_KEY, FILE_PRIVATE_KEY, FILE_REGION)
+
+    # Patch CdpDfClient to avoid real API calls
+    client = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.modules.df_customflow_info.CdpDfClient",
+        autospec=True,
+    ).return_value
+
+    # Mock list_flow_definitions returning empty list
+    client.list_flow_definitions.return_value = {"flows": []}
+
+    # Test module execution
+    with pytest.raises(AnsibleExitJson) as result:
+        df_customflow_info.main()
+
+    assert result.value.changed is False
+    assert len(result.value.flows) == 0
+
+    # Verify CdpDfClient was called correctly
+    client.list_flow_definitions.assert_called_once_with(search_term="NonexistentFlow")
+
+
+def test_df_customflow_info_empty_catalog(module_args, mocker):
+    """Test df_customflow_info module when no flows exist in catalog."""
+
+    module_args(
+        {
+            "endpoint": BASE_URL,
+            "access_key": ACCESS_KEY,
+            "private_key": PRIVATE_KEY,
+        },
+    )
+
+    # Patch load_cdp_config to avoid reading real config files
+    config = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.module_utils.common.load_cdp_config",
+    )
+    config.return_value = (FILE_ACCESS_KEY, FILE_PRIVATE_KEY, FILE_REGION)
+
+    # Patch CdpDfClient to avoid real API calls
+    client = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.modules.df_customflow_info.CdpDfClient",
+        autospec=True,
+    ).return_value
+
+    # Mock list_flow_definitions returning empty list
+    client.list_flow_definitions.return_value = {"flows": []}
+
+    # Test module execution
+    with pytest.raises(AnsibleExitJson) as result:
+        df_customflow_info.main()
+
+    assert result.value.changed is False
+    assert len(result.value.flows) == 0
+
+    # Verify CdpDfClient was called correctly
+    client.list_flow_definitions.assert_called_once()
+
+
+def test_df_customflow_info_with_minimal_fields(module_args, mocker):
+    """Test df_customflow_info module with flows containing minimal fields."""
+
+    module_args(
+        {
+            "endpoint": BASE_URL,
+            "access_key": ACCESS_KEY,
+            "private_key": PRIVATE_KEY,
+        },
+    )
+
+    # Patch load_cdp_config to avoid reading real config files
+    config = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.module_utils.common.load_cdp_config",
+    )
+    config.return_value = (FILE_ACCESS_KEY, FILE_PRIVATE_KEY, FILE_REGION)
+
+    # Patch CdpDfClient to avoid real API calls
+    client = mocker.patch(
+        "ansible_collections.cloudera.cloud.plugins.modules.df_customflow_info.CdpDfClient",
+        autospec=True,
+    ).return_value
+
+    # Mock list_flow_definitions response with minimal fields
+    client.list_flow_definitions.return_value = {
+        "flows": [
+            {
+                "crn": FLOW_CRN,
+                "name": FLOW_NAME,
+                "artifactType": "flow",
+                "versionCount": 1,
+            },
+        ],
+    }
+
+    # Test module execution
+    with pytest.raises(AnsibleExitJson) as result:
+        df_customflow_info.main()
+
+    assert result.value.changed is False
+    assert len(result.value.flows) == 1
+    assert result.value.flows[0]["crn"] == FLOW_CRN
+    assert result.value.flows[0]["name"] == FLOW_NAME
+    assert result.value.flows[0]["versionCount"] == 1
+
+    # Verify optional fields are not present
+    assert "modifiedTimestamp" not in result.value.flows[0]
+    assert "collectionCrn" not in result.value.flows[0]
+    assert "collectionName" not in result.value.flows[0]
+
+    # Verify CdpDfClient was called correctly
+    client.list_flow_definitions.assert_called_once()
+
+
